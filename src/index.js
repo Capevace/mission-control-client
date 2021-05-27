ae81dbc2d81d883fbb7bfd477cf8108cf0d479ac
Lukas Mateffy <luke@mateffy.me>
2021-05-28 00:22:04 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import socketIO from 'socket.io-client';
 import nanobus from 'nanobus';
+import autoBind from 'auto-bind';
 
 /**
  * The error types that cause the {@link SocketEvents} 'error' event to fire.
- * @type {Object} SOCKET_ERROR
+ * @typedef {SOCKET_ERROR}
+ * @enum {string}
  * @property {string} GENERAL A general socket error.
  * @property {string} TIMEOUT The ping to the server timed out.
  * @property {string} NO_ATTEMPTS_LEFT The client ran out of attempts to reconnect to the server.
- * @property {string} AUTH_INVALID_TOKEN The client is not granted access to the server due to the token being invalid.
+ * @property {string} AUTH_FAILED The client is not granted access to the server due to the token being invalid.
  * @property {string} AUTH_TIMEOUT The client is not granted access to the server because the client took too long to authenticate.
  * @since 1.0.0
  */
@@ -15,7 +17,7 @@ export const SOCKET_ERROR = {
 	GENERAL: 'GENERAL',
 	TIMEOUT: 'TIMEOUT',
 	NO_ATTEMPTS_LEFT: 'NO_ATTEMPTS_LEFT',
-	AUTH_INVALID_TOKEN: 'AUTH_INVALID_TOKEN',
+	AUTH_FAILED: 'AUTH_FAILED',
 	AUTH_TIMEOUT: 'AUTH_TIMEOUT'
 };
 
@@ -68,7 +70,7 @@ export class MissionControlClient {
 	 * @param {string} url - The mission control url the client should connect to.
 	 * @param {string} authToken - The JWT authentication token that should be used to authenticate.
 	 */
-	constructor(url, authToken) {
+	constructor(url, authToken, { logger } = {}) {
 		// URL and auth token are required parameters.
 		if (!url) throw new Error('You need to pass an URL.');
 		if (!authToken) throw new Error('You need to pass an Auth Token.');
@@ -100,14 +102,26 @@ export class MissionControlClient {
 		 */
 		this.eventBus = nanobus();
 
-		/** @type {Object<eventKey<string>, listenerCount<Number>>} */
-		this._subscriptions = {};
-		/** @type {Array<eventKey<string>>} */
-		this._subscribeTo = [];
-		/** @type {Array<eventKey<string>>} */
-		this._unsubscribeFrom = [];
+		this.ready = false;
+
+		this._services = {};
+
+		/** @type {Record<string, { listeners: number }>} */
+		this._services = {};
+
+		/** @type {Record<string, object>} */
+		this._stateCache = {};
+
+		const title = ['%cmission-control-client -', 'color:#a78bfa; font-weight: bold; cursor: default; pointer-events: none;user-select: none;'];
+		this.logger = logger || {
+			debug: (...args) => console.log(...title, ...args),
+			warn: (...args) => console.warn(...title, ...args),
+			error: (...args) => console.error(...title, ...args)
+		};
 
 		this._setupSocketHandlers();
+
+		autoBind(this);
 	}
 
 	/**
@@ -138,36 +152,50 @@ export class MissionControlClient {
 					event
 				)
 			)
-				this.eventBus.emit(event, ...args);
+				this.eventBus.emit(`internal:${event}`, ...args);
 		});
 
 		// On connection we try to authenticate
-		this.socket.on('connect', () => {
-			this.socket.emit('authenticate', {
-				token: this.authToken
-			});
-		});
+		this.socket.on('connect', async () => {
+			try {
+				this.ready = false;
 
-		// On successful connection & authentication
-		this.socket.on('authenticated', () => {
-			// While we still have events to subscribe to, do so on connect
-			while (this._subscribeTo.length > 0) {
-				this.socket.emit('subscribe', {
-					event: this._subscribeTo.shift()
+				this.logger.debug('auth:start');
+				
+				const response = await this._socketEmit('authenticate', {
+					token: this.authToken
 				});
-			}
 
-			// While we still have events to unsubscribe from, do so on connect
-			while (this._unsubscribeFrom.length > 0) {
-				this.socket.emit('unsubscribe', {
-					event: this._unsubscribeFrom.shift()
-				});
-			}
+				this.ready = true;
+				
+				this.logger.debug('auth:complete', response);
+				this.logger.debug('ready');
 
-			// We simplify the event structure here, by emitting a 'connect' event rather than a 'authenticated' event.
-			// This essentially hides the implementation details of the authentication flow from the client user
-			// which is what we're aiming for by making the client simple.
-			this.eventBus.emit('connect');
+				// Re-Subscribe to all services after reconnect
+				for (const service in this._services) {
+					this.logger.debug('subscribe to service:', service);
+
+					this._socketEmit('subscribe', { service })
+						.catch(this.reportError);
+				}
+
+				// // While we still have events to unsubscribe from, do so on connect
+				// while (this._unsubscribeFrom.length > 0) {
+				// 	this._socketEmit('unsubscribe', {
+				// 		service: this._unsubscribeFrom.shift()
+				// 	}).then(console.info).catch(console.error);
+				// }
+
+				// We simplify the event structure here, by emitting a 'connect' event rather than a 'ready' event.
+				// This essentially hides the implementation details of the authentication flow from the client user
+				// which is what we're aiming for by making the client simple.
+
+				this.eventBus.emit('connect');
+			} catch (e) {
+				this.ready = false;
+
+				this.eventBus.emit('error', SOCKET_ERROR.AUTH_FAILED, e);
+			}
 		});
 
 		// On disconnect from server, reason can either be server disconnect, client disconnect or ping timeout.
@@ -188,27 +216,55 @@ export class MissionControlClient {
 					disconnectReason = DISCONNECT_REASON.UNKNOWN;
 			}
 
+			this.ready = false;
+
 			this.eventBus.emit('disconnect', disconnectReason);
 		});
 
 		/*
 		 * RECONNECTION
+		 *
+		 * On successful reconnect, attempt is the amount of attempts needed for the reconnect.
+		 * As of right now, not really needed for anything,
+		 * as the connect event fires on every successful connect, even reconnects.
+		 * this.socket.on('reconnect', attempt => {});
 		 */
-		// On successful reconnect, attempt is the amount of attempts needed for the reconnect.
-		// As of right now, not really needed for anything,
-		// as the connect event fires on every successful connect, even reconnects.
-		// this.socket.on('reconnect', attempt => {});
 
 		// On reconnect attempt, attempt is the current attempt number
 		this.socket.on('reconnect_attempt', attempt => {
+			this.ready = false;
 			this.eventBus.emit('reconnecting', attempt);
+		});
+
+		/*
+		 * SYNC
+		 */
+		this.socket.on('sync', ({ service: serviceName, state }) => {
+			try {
+				this.logger.debug('sync – service:', serviceName, 'state:', state);
+
+				if (!this._services[serviceName]) {
+					return this.logger.warn('received sync update for unsubscribed service', serviceName, state);
+				}
+
+				this._stateCache[serviceName] = state;
+				this.eventBus.emit(`sync:${serviceName}`, state);
+			} catch (e) {
+				this.reportError(e);
+			}
 		});
 
 		/*
 		 * ERROR HANDLING
 		 */
+		this._setupErrorHandlers();
+	}
+
+	_setupErrorHandlers() {
 		// On a general connection error, the error object is the error thrown
 		this.socket.on('connect_error', error => {
+			this.ready = false;
+
 			// TODO: determine errorType
 			this.eventBus.emit('error', SOCKET_ERROR.GENERAL, error);
 		});
@@ -216,18 +272,17 @@ export class MissionControlClient {
 		// On a ping/connection timeout error, the timeout object is IDK what
 		// TODO: what is the timeout object?
 		this.socket.on('connect_timeout', timeout => {
+			this.ready = false;
+
 			this.eventBus.emit('error', SOCKET_ERROR.TIMEOUT, timeout);
 		});
 
 		// Called when we can't authenticate because of an invalid auth token or because
 		// the client took too long to authenticate.
-		this.socket.on('unauthorized', error => {
-			const authErrorType =
-				error.type === 'TIMEOUT'
-					? SOCKET_ERROR.AUTH_TIMEOUT
-					: SOCKET_ERROR.AUTH_INVALID_TOKEN;
+		this.socket.on('authentication_timeout', ({ error }) => {
+			this.ready = false;
 
-			this.eventBus.emit('error', authErrorType, error);
+			this.eventBus.emit('error', SOCKET_ERROR.AUTH_TIMEOUT, error);
 		});
 
 		// On reconnect error, dont know if needed for now
@@ -236,8 +291,32 @@ export class MissionControlClient {
 		// On reconnection failed, fired becayse we run out of attempts
 		// and not because there is an error in the connection
 		this.socket.on('reconnect_failed', () => {
+			this.ready = false;
+
 			this.eventBus.emit('error', SOCKET_ERROR.NO_ATTEMPTS_LEFT);
 		});
+	}
+
+	/**
+	 * Internal socket emit helper
+	 * @async
+	 * @protected
+	 * @param  {string} event Event to emit
+	 * @param  {[type]} data  Data to send
+	 * @return {object}       Response data
+	 * @throws Error
+	 */
+	_socketEmit(event, data) {
+		return new Promise((resolve, reject) => {
+			this.socket.emit(event, data, (response) => {
+				if (response.error) {
+					return reject(response.error);
+				}
+
+				resolve(response);
+			});
+		});
+
 	}
 
 	/**
@@ -301,37 +380,84 @@ export class MissionControlClient {
 	 * client.subscribe('action:EXAMPLE:DO', (data) => {})
 	 * client.subscribe('update:stateObject', (data) => {});
 	 */
-	subscribe(serverEvent, listener) {
-		this.socket.on(serverEvent, listener);
+	service(name, listener) {
+		const errorHandledListener = async (state) => {
+			try {
+				await listener(state);
+			} catch (e) {
+				this.logger.error('error in service state handler ' + name, e);
 
-		if (!(serverEvent in this._subscriptions)) {
-			if (this.socket.connected) {
-				this.socket.emit('subscribe', {
-					event: serverEvent
-				});
-				this._subscriptions[serverEvent] = 1;
-			} else {
-				this._subscribeTo.push(serverEvent);
+				this.reportError(e);
+			}
+		};
+
+		this.eventBus.on(`sync:${name}`, errorHandledListener);
+
+		// If we find "older" state in the cache, we send that for first paint
+		const staleState = this._stateCache[name];
+		if (staleState) {
+			this.logger.debug('found stale state for service', name);
+			errorHandledListener(staleState);
+		}
+
+		if (name in this._services) {
+			this.logger.debug('already subscribed to sync from', name);
+
+			this._services[name].listeners++;
+		} else {
+			this._services[name] = { 
+				listeners: 1
+			};
+
+			// If the client is ready, that means
+			// it has already started subscriptions for things in this._services.
+			// We have to connect manually here.
+			// However this listener is registered in this._services now
+			// so on next automatic resubscribe (after disconnect) it will be included.
+			if (this.ready) {
+				this.logger.debug('subscribe to service:', name);
+
+				this._socketEmit('subscribe', {
+					service: name
+				}).catch(this.reportError);
 			}
 		}
 
-		return () => {
-			this.socket.removeListener(serverEvent, listener);
+		const unsubscribe = () => {
+			this.logger.debug('listener unsubscribed from service', name);
 
-			if (this.socket.connected) {
-				this._subscriptions[serverEvent]--;
+			this.eventBus.removeListener(`sync:${name}`, listener);
+			this._services[name].listeners--;
 
-				if (this._subscriptions[serverEvent] === 0) {
-					delete this._subscriptions[serverEvent];
+			if (this._services[name].listeners <= 0) {
+				this.logger.debug('removing service from sync', name);
 
-					this.socket.emit('unsubscribe', {
-						event: serverEvent
-					});
+				delete this._services[name];
+
+				if (this.socket.ready) {
+					this._socketEmit('unsubscribe', {
+						service: name
+					}).catch(this.reportError);
 				}
-			} else {
-				this._unsubscribeFrom.push(serverEvent);
 			}
 		};
+
+		// Getters (ready, state) don't allow arrow functions
+		// so we have the good old JS 'this' problem again.
+		const _this = this;
+
+		return {
+			get ready() {
+				return _this._stateCache[name] !== null;
+			},
+			async action(actionName, data) {
+				await _this.action(name, actionName, data);
+			},
+			get state() {
+				return _this._stateCache[name] || null;
+			},
+			unsubscribe
+		}
 	}
 
 	/**
@@ -344,11 +470,30 @@ export class MissionControlClient {
 	 * @param {object} data - The data you want to pass to the action function.
 	 *
 	 * @since 1.0.0
+	 * @async
 	 * @example
 	 * client.action('EXAMPLE:DO', { parameter: 'example' })
 	 * client.action('VIDEO-QUEUE:PUSH', { video: { url: '...', format: 'mp4' }})
 	 */
-	action(action, data) {
-		this.socket.emit('action', { action, data });
+	action(service, action, data) {
+		return new Promise((resolve, reject) => {
+			this.socket.emit('action', { service, action, data }, (res) => {
+				if (res.error) {
+					reject(new Error(res.error.message));
+				} else {
+					resolve(res);
+				}
+			});
+		});
+	}
+
+	/**
+	 * Report a general error to the user of the library
+	 * @param {Error} error The error object
+	 */
+	reportError(error) {
+		this.logger.error('error reported', error);
+
+		this.eventBus.emit('error', SOCKET_ERROR.GENERAL, error);
 	}
 }
